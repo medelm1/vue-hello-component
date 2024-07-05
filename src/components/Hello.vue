c35877fa4ac5c851f1a03665635d8242198be5ff
Med Elm <elmehammedi.mohamed@gmail.com>
2024-07-05 02:06:55 +0100

    correct identations

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="hello">
-    <h1>{{ greeting }}, {{ name }}!</h1>
-    <button @click="handleClick">Click Me</button>
-  </div>
+    <div class="hello">
+        <h1>{{ greeting }}, {{ name }}!</h1>
+        <button @click="handleClick">Click Me</button>
+    </div>
 </template>
-
 <script setup>
 import { defineProps, defineEmits, ref } from 'vue'
 
 // Define props
 const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  },
-  greeting: {
-    type: String,
-    default: 'Hello'
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    greeting: {
+        type: String,
+        default: 'Hello'
+    }
 })
 
 // Define emits
@@ -28,34 +27,33 @@ const counter = ref(0)
 
 // Methods
 const handleClick = () => {
-  counter.value++
-  emit('clicked', counter.value)
+    counter.value++
+    emit('clicked', counter.value)
 }
 </script>
-
 <style scoped>
 .hello {
-  text-align: center;
-  margin-top: 20px;
+    text-align: center;
+    margin-top: 20px;
 }
 
 .hello h1 {
-  font-size: 2rem;
-  color: #42b983;
+    font-size: 2rem;
+    color: #42b983;
 }
 
 .hello button {
-  margin-top: 10px;
-  padding: 10px 20px;
-  font-size: 1rem;
-  color: white;
-  background-color: #42b983;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
+    margin-top: 10px;
+    padding: 10px 20px;
+    font-size: 1rem;
+    color: white;
+    background-color: #42b983;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
 }
 
 .hello button:hover {
-  background-color: #2c8e6f;
+    background-color: #2c8e6f;
 }
 </style>
